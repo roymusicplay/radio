62ed72006de3d8aebd2bca8dad8475abc91eb6f6
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-08-17 22:02:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3 /radio
-cd /RadioPlayerV3
+git clone https://github.com/roymusicplay/radio /radio
+cd /radio
 echo "Starting Bot, Please Wait..."
 python3 main.py
