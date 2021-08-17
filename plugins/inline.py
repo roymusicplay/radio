e529e15301955d1b6e67a27631c79fb6f5cf21ca
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-08-17 21:10:51 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -30,11 +30,11 @@ buttons = [
                 InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
             ],
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/ABOUT_ABHINAS"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/DOSTI_GROUP_1234"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/RadioPlayerV3"),
+                InlineKeyboardButton("🥰 Father 🥰", url="https://t.me/abhinasroy"),
             ]
          ]
 
@@ -46,8 +46,8 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy Your Own Radio Player",
-                thumb_url="https://telegra.ph/file/4e839766d45935998e9c6.jpg",
-                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@AsmSafone | @SafoTheBot 👑</b>", disable_web_page_preview=True),
+                thumb_url="https://telegra.ph/file/d404ce5e006643c809eaa.jpg",
+                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@abhinasroy | @ABOUT_ABHINAS 👑</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
